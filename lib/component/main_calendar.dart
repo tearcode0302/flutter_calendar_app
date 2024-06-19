3a154e8228410e156a8f83d7cd7b912b54f4d3f7
sangjun <byjun0302@gmail.com>
2024-06-19 14:27:26 +0900

    feat: edit mainCalendar color style

--- lib/component/main_calendar.dart
+++ lib/component/main_calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_app/const/colors.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MainCalendar extends StatelessWidget {
@@ -10,6 +11,44 @@ class MainCalendar extends StatelessWidget {
       focusedDay: DateTime.now(),
       firstDay: DateTime(1800, 1, 1),
       lastDay: DateTime(3000, 1, 1),
+      headerStyle: HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: false,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false,
+        defaultDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(9.0),
+          color: Color(0xFFEDE7F6),
+        ),
+        weekendDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(9.0),
+          color: Color(0xFF90CAF9)
+        ),
+        selectedDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(9.0),
+          border: Border.all(
+            color: Color(0xFFEDE7F6),
+            width: 1.0,
+          ),
+        ),
+        defaultTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR
+        ),
+        weekendTextStyle: TextStyle(
+          fontWeight: FontWeight.w900,
+          color: DARK_GREY_COLOR
+        ),
+        selectedTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: PRIMARY_COLOR,
+        )
+      ),
     );
   }
 }
