8ff8e3e4f6ab6a3a3bf78366de38485d82c4b855
sangjun <byjun0302@gmail.com>
2024-06-19 17:04:52 +0900

    feat: make TableCalendar to MainCalendar

--- lib/component/main_calendar.dart
+++ lib/component/main_calendar.dart
@@ -3,11 +3,23 @@ import 'package:flutter_calendar_app/const/colors.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MainCalendar extends StatelessWidget {
-  const MainCalendar({super.key});
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDate;
+
+  const MainCalendar({
+    super.key,
+    required this.onDaySelected,
+    required this.selectedDate,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
+      onDaySelected: onDaySelected,
+      selectedDayPredicate: (date) =>
+        date.year == selectedDate.year &&
+        date.month == selectedDate.month &&
+        date.day == selectedDate.day,
       focusedDay: DateTime.now(),
       firstDay: DateTime(1800, 1, 1),
       lastDay: DateTime(3000, 1, 1),
@@ -20,35 +32,30 @@ class MainCalendar extends StatelessWidget {
         ),
       ),
       calendarStyle: CalendarStyle(
-        isTodayHighlighted: false,
-        defaultDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(9.0),
-          color: Color(0xFFEDE7F6),
-        ),
-        weekendDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(9.0),
-          color: Color(0xFF90CAF9)
-        ),
-        selectedDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(9.0),
-          border: Border.all(
+          isTodayHighlighted: false,
+          defaultDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(9.0),
             color: Color(0xFFEDE7F6),
-            width: 1.0,
           ),
-        ),
-        defaultTextStyle: TextStyle(
-          fontWeight: FontWeight.w600,
-          color: DARK_GREY_COLOR
-        ),
-        weekendTextStyle: TextStyle(
-          fontWeight: FontWeight.w900,
-          color: DARK_GREY_COLOR
-        ),
-        selectedTextStyle: TextStyle(
-          fontWeight: FontWeight.w600,
-          color: PRIMARY_COLOR,
-        )
-      ),
+          weekendDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(9.0),
+              color: Color(0xFF90CAF9)),
+          selectedDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(9.0),
+            color: Colors.red,
+            border: Border.all(
+              color: Color(0xFFF37021),
+              width: 1.0,
+            ),
+          ),
+          defaultTextStyle:
+              TextStyle(fontWeight: FontWeight.w600, color: DARK_GREY_COLOR),
+          weekendTextStyle:
+              TextStyle(fontWeight: FontWeight.w900, color: DARK_GREY_COLOR),
+          selectedTextStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: PRIMARY_COLOR,
+          )),
     );
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_app/component/main_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          MainCalendar(),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+          ],
+        ),
       )
     );
   }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
 }
+
